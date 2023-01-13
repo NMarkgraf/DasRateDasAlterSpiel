51f1086bcf828f0256ba0d55dd137a82a5679678
NMarkgraf <nmarkgraf@hotmail.com>
2023-01-13 08:36:47 +0100

    - Copyright hinzugefügt - etwas mehr dokumentiert - jetzt wird D[0:i] in i durchlaufen und für jedes i ein neues Säulendiagramm ausgegeben. So kann die Entscheidung besser verfolgt werden.

--- spiel.py
+++ spiel.py
@@ -1,33 +1,67 @@
 """
-Spiel. Errate das Alter
+Spiel. Errate das Alter!
 
-a) Jeder im Raum schreibt sein Alter auf einen Zettel und wirft ihn in die Urne.
-b) Jemand zieht einen Zettel. Das Alter x, welches gezogen wird soll erraten werden.
-c) Nun wird n=20 mal aus der Urne (mit zurücklegen) gezogen und der neue Wert y
+    (C)opyright in 2023 by N. Markgraf (nmarkgraf@hotmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+a) Jeder im Raum schreibt sein Alter auf einen Zettel und wirft ihn in eine Urne.
+b) Jemand zieht einen Zettel. Das Alter x auf diesem Zettel soll erraten werden.
+c) Nun wird n(=20 ?) mal aus der Urne (mit zurücklegen) gezogen und der neue Wert y
    mit dem Wert x verglichen. Es können drei mögliche Ergebnisse auftreten.
-   x < y, x = y oder x > y.
-   Die Ergebnisse "<", "=" und ">" werden notiert. (Und D eingetragen)
+   y < x, y = x oder y > x.
+   Die Ergebnisse dieses Vergleichs ("<", "=" und ">") werden notiert. (Und hier unter D eingetragen!)
 d) Dieses Programm wird auf Grundlage von T, Tn und D die Wahrscheinlichkeiten
    auf Grundlage der Regel von Bayes bestimmen.
 
 """
 from fractions import Fraction
 import matplotlib.pyplot as plt
+""" 
+    T gibt den Wertebereich an
+"""
+T =  (22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42)
 
-T =  [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
-Tn = [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
-Tn = [ 2,  1,  1,  3,  0,  1,  2,  0,  1,  0,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2]
+"""
+    Tn gibt an, wie oft der jeweilige Wert von T vorkommt
+"""
+# Keine a priori Annahme:
+Tn = ( 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1)
+
+# relative Häufigkeiten als a priori Annahme:
+Tn = ( 2,  1,  1,  3,  0,  1,  2,  0,  1,  0,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2)
+
+"""
+    N liefert die tatsächliche Anzahl an Werten
+"""
 N = sum(Tn)
 
 """
-T  = [1, 2, 3, 4, 5, 6]
-Tn = [1, 1, 1, 1, 1, 1]
+Beispiel für einen Würfel (W6) zum Testen:
+"""
+"""
+T  = (1, 2, 3, 4, 5, 6)
+Tn = (1, 1, 1, 1, 1, 1)
 N = sum(Tn)
 """
 
-D = ("<", ">", ">", ">") #, "=", "=", "<", ">", ">", "<",
-#     "=", ">", ">", "<", ">", ">", ">", "=", "=", "<",
-#     ">", "=", ">", ">", "=", "=", ">", "=", ">", "<")
+"""
+    In D speichern wir die Anfolge an "<", ">" und "=" aus den Vergleichen.
+"""
+D = ("<", ">", ">", ">", "=", "=", "<", ">", ">", "<",
+     "=", ">", ">", "<", ">", ">", ">", "=", "=", "<",
+     ">", "=", ">", ">", "=", "=", ">", "=", ">", "<")
 
 def berechne_p_von_T(T, Tn):
     """
@@ -41,7 +75,7 @@ def berechne_p_von_T(T, Tn):
     """
     if len(T) != len(Tn):
         raise ValueError("!!!")
-    return [Fraction(x, N) for x in Tn]
+    return tuple(Fraction(x, N) for x in Tn)
 
 
 def berechne_p_von_D_gegeben_T(D, T, Tn):
@@ -53,28 +87,29 @@ def berechne_p_von_D_gegeben_T(D, T, Tn):
     :return: P(D | T = i) für alle i aus T
     """
     ret = []
-    for i in range(0, len(T)):
+    #for i in range(0, len(T)):
+    for i, _ in enumerate(T):
         p = Fraction(1, 1)
         for d in D:
-            # Für T = i bestimme p(d | T = i)
-            g = sum(Tn[i+1:])
-            l = sum(Tn[:i])
-            e = Tn[i]
+            # Für T = i bestimme p(d | T = i) und multipliziere die W'keiten in p
             if d == "<":
-                p *= Fraction(l, N)
+                p *= Fraction(sum(Tn[:i]), N)
             if d == ">":
-                p *= Fraction(g, N)
+                p *= Fraction(sum(Tn[i+1:]), N)
             if d == "=":
-                p *= Fraction(e, N)
+                p *= Fraction(Tn[i], N)
+        # Erbebnis anhängen und nächstes T wählen
         ret += [p]
     return ret
 
 
 def berechne_p_von_D(pT, pDT):
     """
-    Berechne p(D), also die totale Wahrscheinlichkeit
+    Berechne p(D), also die totale Wahrscheinlichkeit von D
 
-    :return:
+    p(D) = p(T=1) * p(D | T=1) + p(T=1) * p(D | T=2) * ... * p(T=N) * p(D | T=N)
+
+    :return: p(D)
     """
     ret = Fraction(0, 1)
     for pt, pdt in zip(pT, pDT):
@@ -84,12 +119,14 @@ def berechne_p_von_D(pT, pDT):
 
 def berechne_p_von_T_gegeben_D(pT, pDT, pD):
     """
-    Nutze die Bayes Formel um p(T | D) zu berechnen
+    Nutze die Bayes-Regel um p(T | D) aus p(T), P(D) und P(D | T)zu berechnen:
 
-    :param pT:
-    :param pDT:
-    :param pD:
-    :return:
+    p(T | D) = p(T) * p(D | T)  / p(D)
+
+    :param pT: p(T)
+    :param pDT: p(D | T)
+    :param pD: p(D)
+    :return: p(T | D)
     """
     ret = []
     for pt, pdt in zip(pT, pDT):
@@ -97,18 +134,82 @@ def berechne_p_von_T_gegeben_D(pT, pDT, pD):
     return ret
 
 
+def show_p_von_T(pT):
+    """
+    Zeige p(T) als Säulendiagramm an
+
+    :param pT: p(T)
+    """
+    ax = plt.subplot()
+    ax.bar(T, pT)
+    ax.set_ylabel('P(T)')
+    ax.set_title('A priorie Wahrscheinlichkeiten von T')
+    plt.show()
+
+def show_p_von_D_von_T(pDT):
+    """
+    Zeige p(D | T) als Säulendiagramm an
+
+    :param pT: p(T)
+    """
+    ax = plt.subplot()
+    ax.bar(T, pDT)
+    ax.set_ylabel('P(D | T)')
+    ax.set_title('Wahrscheinlichkeiten von D gegeben T')
+    plt.show()
+
+def show_p_von_T_von_D(pTD, n=0):
+    """
+    Zeige p(T | D) als Säulendiagramm an
+
+    :param pT: p(T)
+    """
+    ax = plt.subplot()
+    ax.bar(T, pTD)
+    ax.set_ylabel('P(T | D)')
+    #ax.set_title('Wahrscheinlichkeiten von T gegeben D')
+    if n > 0:
+        ax.set_title(f'Wahrscheinlichkeiten von T gegeben D[0:{n}] auf Grundlage der Bayes-Regel')
+    else:
+        ax.set_title(f'Wahrscheinlichkeiten von T gegeben D auf Grundlage der Bayes-Regel')
+
+    plt.show()
+
+
 def main():
     """
-    Hauptroutine
+        Hauptroutine
     """
     p_T = berechne_p_von_T(T, Tn)
     print("p(T) =", p_T)
+    show_p_von_T(p_T)
 
+    for i, _  in enumerate(D,1):
+        d = D[:i]
+
+        p_D_T = berechne_p_von_D_gegeben_T(d, T, Tn)
+        print("p(D | T=i) =", p_D_T)
+        #show_p_von_D_von_T(p_D_T)
+
+        p_D = berechne_p_von_D(p_T, p_D_T)
+        print("p(D) =", p_D)
+
+        p_T_D = berechne_p_von_T_gegeben_D(p_T, p_D_T, p_D)
+        print("p(T | D) =", p_T_D)
+
+        p = [float(x) for x in p_T_D]
+        print("p(T | D) =", p)
+
+        show_p_von_T_von_D(p_T_D, i)
+
+"""
     p_D_T = berechne_p_von_D_gegeben_T(D, T, Tn)
-    print("p(D|T=i) =", p_D_T)
+    print("p(D | T=i) =", p_D_T)
+    show_p_von_D_von_T(p_D_T)
 
     p_D = berechne_p_von_D(p_T, p_D_T)
     print("p(D) =", p_D)
+    
 
     p_T_D = berechne_p_von_T_gegeben_D(p_T, p_D_T, p_D)
     print("p(T | D) =", p_T_D)
@@ -121,7 +222,7 @@ def main():
     ax.set_ylabel('P(T = x | D)')
     ax.set_title('Wahrscheinlichkeiten mit Bayes auf Grundlage von D')
     plt.show()
-
+"""
 
 if __name__ == "__main__":
     main()
